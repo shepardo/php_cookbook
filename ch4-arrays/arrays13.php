08af4aab867e045a3b9ca761976d725d3bb7d875
Fernando Gonzalez Sanchez <fernando.gonzalez@wizeline.com>
2020-10-10 00:09:25 -0500

    Fixes for last script.

--- ch4-arrays/arrays13.php
+++ ch4-arrays/arrays13.php
@@ -5,22 +5,29 @@ Problem
 You want to locate entries in an array that meet certain requirements.
 */
 $movies = array(
-    'Empire Strikes Back', 'Han Solo Story', 'The Phantom Menace', 
-    'The Return of the Jedi', 'The Clone Wars', 'The Revenge of the Sith');
+    ['title' => 'Empire Strikes Back', 'box_office_gross' => 10000000 ], 
+    ['title' => 'Han Solo Story', 'box_office_gross' => 2000000 ], 
+    ['title' => 'The Phantom Menace', 'box_office_gross' => 35000000], 
+    ['title' => 'The Return of the Jedi', 'box_office_gross' => 50000000], 
+    ['title' => 'The Clone Wars', 'box_office_gross' => 40000000],
+    ['title' => 'The Revenge of the Sith', 'box_office_gross' => 3500000]);
 $flops = [];    
-function search1($movies) {
+function search1($movies, $flops) {
   foreach ($movies as $movie) {
-    if ($movie['box_office_gross'] < 5000000) { $flops[] = $movie; }
-    }
+    if ($movie['box_office_gross'] > 5000000) { $flops[] = $movie; }
+  }
+  $GLOBALS['flops'] = $flops;
 }
 
-function search2($movies) {
+function search2($movies, &$flops) {
   $flops = array_filter($movies, function ($movie) {
-    return ($movie['box_office_gross'] < 5000000) ? 1 : 0;
+    return ($movie['box_office_gross'] > 5000000) ? 1 : 0;
   });
 }
-print search1($movies) . "\n";
-search2($movies2);
+search1($movies, $flops);
+print_r($flops);
+$flops = [];
+search2($movies, $flops);
 print_r($flops);
 
 ?>
