4d9199275e2a908d10234f5bf6aebb243f3ab4b9
Fernando Gonzalez Sanchez <fernando.gonzalez@wizeline.com>
2020-10-13 23:58:53 -0500

    another script.

---
+++ ch5-variables/arrays4.php
@@ -0,0 +1,9 @@
+<?php
+/*
+5.4 Creating a Dynamic Variable Name
+Problem
+You want to construct a variable’s name dynamically. For example, you want to use
+variable names that match the field names from a database query.
+*/
+
+?>
