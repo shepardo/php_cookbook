74a0adb9ddf986910a3d98a41b6cd57bfa6050f1
Fernando Gonzalez Sanchez <fernando.gonzalez@wizeline.com>
2020-11-01 00:17:03 -0600

    Adding another script to Ch 8 - Web programming.

---
+++ ch8-web/web1.php
@@ -0,0 +1,11 @@
+<?php
+/*
+8.1 Setting Cookies
+Problem
+You want to set a cookie so that your website can recognize subsequent requests from
+the same web browser.
+
+Run web server with php -S 0.0.0.0:80000
+*/
+setcookie('flavor','chocolate chip');
+?>
